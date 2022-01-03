4a5e849b123b6ddb6d402cde9b7776adcc280de2
kuhm01 <kuhm01@naver.com>
2022-01-03 11:28:31 +0900

    조건 표현식 진행 중

--- ast/ast.go
+++ ast/ast.go
@@ -40,6 +40,11 @@ type ExpressionStatement struct {
 	Expression Expression
 }
 
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
@@ -51,6 +56,13 @@ type InfixExpression struct {
 	Left     Expression
 	Operator string
 	Right    Expression
+}
+
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
 }
 
 type Identifier struct {
@@ -170,3 +182,33 @@ func (ie *InfixExpression) String() string {
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+func (bs *BlockStatement) statementNode() {}
+func (bs *BlockStatement) TokenLiteral()  { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
