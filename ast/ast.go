6ad7e14ee5351a252868ec8710576402e5691a81
kuhm01 <kuhm01@naver.com>
2022-01-10 02:27:35 +0900

    해시 파싱 : 작성 완료 + 테스트케이스 불통과

--- ast/ast.go
+++ ast/ast.go
@@ -103,6 +103,11 @@ type ArrayLiteral struct {
 	Elements []Expression
 }
 
+type HashLiteral struct {
+	Token token.Token
+	Pairs map[Expression]Expression
+}
+
 type IndexExpression struct {
 	Token token.Token
 	Left  Expression
@@ -310,6 +315,23 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("[")
 	out.WriteString(ie.Index.String())
 	out.WriteString("])")
+
+	return out.String()
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
