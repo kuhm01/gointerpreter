12010e916ff29ff3c328726b54159a4e0b4a8089
kuhm01 <kuhm01@naver.com>
2022-01-10 15:59:02 +0900

    solving builtins.go problem

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,7 +6,7 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -23,7 +23,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"first": &object.Builtin{
+	"first": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -43,7 +43,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"last": &object.Builtin{
+	"last": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -64,7 +64,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"rest": &object.Builtin{
+	"rest": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
@@ -78,7 +78,7 @@ var builtins = map[string]*object.Builtin{
 			length := len(arr.Elements)
 
 			if length > 0 {
-				newElements := make([]object.Object, length-1, length-1)
+				newElements := make([]object.Object, length-1) //By Book. make([]object.Object, length-1, length-1)
 				copy(newElements, arr.Elements[1:length])
 				return &object.Array{Elements: newElements}
 			}
@@ -87,7 +87,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"push": &object.Builtin{
+	"push": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 2 {
 				return newError("wrong number of arguments. got=%d, want=2", len(args))
@@ -100,7 +100,7 @@ var builtins = map[string]*object.Builtin{
 			arr := args[0].(*object.Array)
 			length := len(arr.Elements)
 
-			newElements := make([]object.Object, length+1, length+1)
+			newElements := make([]object.Object, length+1) //By Book. make([]object.Object, length+1, length+1)
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 
@@ -108,7 +108,7 @@ var builtins = map[string]*object.Builtin{
 		},
 	},
 
-	"puts": &object.Builtin{
+	"puts": {
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
 				fmt.Println(arg.Inspect())
