b68721db6ecedcc2254381a6008c3adf8b6407ac
kuhm01 <kuhm01@naver.com>
2022-01-10 15:44:16 +0900

    4장 인터프리터 확장 - 그랜드 파이널 : 완료

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/object"
 )
 
@@ -104,6 +105,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
